77038ef4253e046f379361fa43dacfc3e7a461d1
jaffec <jaffec@gilmour.org>
2017-09-22 14:04:03 -0400

    hello robot

--- main.py
+++ main.py
@@ -4,17 +4,42 @@ import time
 class Piggy(object):
 
     def __init__(self):
+        print("I AM ALIVE")
 
-        print("Hello sir, I am your robot")
-        self.dance()
+    def pulse(self):
+        """check for obstacles, drive fixed amount forward"""
+        look = us_dist(15)  # store the distance reading
+        if look > 80:
+            fwd()
+            time.sleep(1)
+            stop()
 
-    def dance(self):
+    def cruise(self):
+        """drive fwd, stop if sensor detects obstacle"""
         fwd()
-        time.sleep(1.5)
-        right.rot()
-        time.sleep(.5)
-        stop()
+        while(True):
+            if us_dist(15) < 30:
+                stop()
+            time.sleep(.2)
 
-##########app
+    def servo_sweep(self):
+        """loops in a 120 degree arc and moves servo"""
+        for ang in range(20, 160, 2):
+            servo(ang)
+            time.sleep(.2)
 
-lucky = Piggy()
+
+
+
+    def cha_cha(self):
+        for x in range(5):
+            right_rot()
+            time.sleep(.5)
+            left_rot()
+            time.sleep(.5)
+            motor_fwd()
+
+            stop()
+
+
+p = Piggy()
